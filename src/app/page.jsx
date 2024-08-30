0f3507d68ae8376874911f2f5d57f5686c651857
okeM420 <thiscaodeiro@gmail.com>
2024-08-30 13:00:21 -0300

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,39 +7,47 @@ export default function Home() {
   return (
     <>
       <header className={styles.header}>
-        <Image alt="logo" src="/softtek-logo.png" width={250} height={100} />
+        <Image alt="logo" src="/softtek-logo.png" width={100} height={50} />
       </header>
 
       <main className={styles.main}>
         <section className={styles.hero}>
           <h1>Bem-vindo à AgroAI</h1>
+        </section>
+
+        <section className={styles.description}>
           <p>
-          Nosso sistema de gerenciamento de chamados oferece uma solução completa para a organização e resolução de solicitações de suporte. Com nossa plataforma, você pode:
+            Nosso sistema de gerenciamento de chamados oferece uma solução
+            completa para a organização e resolução de solicitações de suporte.
+            Com nossa plataforma, você pode:
           </p>
         </section>
+
         <section className={styles.features}>
           <div className={styles.featureList}>
             <div className={styles.featureItem}>
-              <h3>Chamados</h3>
-              <p>Visualizar e responder chamados existentes.</p>
+              <h3>Visualizar Chamados</h3>
+              <p> Acesso a todos os chamados existentes.</p>
             </div>
             <div className={styles.featureItem}>
-              <h3>Dashbords</h3>
-              <p>Acessar e criar Dashboards para analisar as métricas dos atendimentos.</p>
+              <h3>Acessar Dashboards</h3>
+              <p>
+                Analisar as métricas dos
+                atendimentos.
+              </p>
             </div>
             <div className={styles.featureItem}>
               <h3>AgroAI</h3>
-              <p>Inteligencia Artificial para auxilio nos atendimentos.</p>
+              <p>Inteligência Artificial para auxílio nos atendimentos.</p>
             </div>
           </div>
-          
         </section>
-        
+
         <Link href="/chats">
-            <button className={styles.button}> 
-              Começar <span>&rarr;</span>
-            </button>
-          </Link>
+          <button className={styles.button}>
+            Começar <span>&rarr;</span>
+          </button>
+        </Link>
       </main>
 
       <footer className={styles.footer}>
